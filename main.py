caab363e6e71121abcd37eb6a88c58d7c320f1ba
Jack Westmore <jack.westmore@housekeep.com>
2025-07-31 08:23:51 +0100

    Add pre-commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def run_file(filename: str) -> None:
+    print(f'run_file() ({filename})')
+
+
+def run_prompt() -> None:
+    print('run_prompt()')
+
+
+def main():
+    if len(sys.argv) > 2:
+        print("Usage: python main.py <filename>")
+        sys.exit(64)
+    elif len(sys.argv) == 2:
+        filename = sys.argv[1]
+        run_file(filename)
+    else:
+        run_prompt()
+
+
+if __name__ == '__main__':
+    main()
